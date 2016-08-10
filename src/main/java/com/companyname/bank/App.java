c1f2061fc9c0500c9316be5144777e192d3de94b
dhavleakshay <akshaydhavle99@gmail.com>
2016-08-10 11:43:55 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -16,6 +16,6 @@ public class App {
 	 */
     public static void main( String[] args )
     {
-        System.out.println( "Devops training!" );
+        System.out.println( "Dev training!" );
     }
 }
